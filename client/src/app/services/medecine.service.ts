15b260ee8d475fc13de2d5b8cc07f145b1ce4073
Lchamodya <73007235+Lchamodya@users.noreply.github.com>
2023-11-14 13:14:48 +0530

    Update medecine.service.ts

--- client/src/app/services/medecine.service.ts
+++ client/src/app/services/medecine.service.ts
@@ -131,7 +131,6 @@ export class MedicineService {
       if (socketUpdate) {
         return {
           ...existingMedicine,
-          // Merge other properties as needed
         };
       } else {
         return existingMedicine;
